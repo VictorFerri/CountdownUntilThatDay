96ee8b67082e497ded56e47fafdc30fef43a129a
VictorFerri <victorferri_@hotmail.com>
2023-11-22 13:26:06 -0300

    Create and styling the header

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+function Header() {
+    return (
+        <header className="header">
+            <ul className="options">
+                <li className="option">
+                    Quem somos
+                </li>
+                <li className="option">
+                    Projetos
+                </li>
+                <li className="option">
+                    Contato
+                </li>
+                <li className="option">
+                    Trabalhe conosco
+                </li>
+            </ul>
+
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,19 @@
+import { Button } from "antd";
+
+import '../../styles/sass/app.scss';
+import Header from "../../components/Header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Button type="secondary">Testesdf</Button>
+            </Header>
+        </div>
+    );
+}
+
+export default App;
